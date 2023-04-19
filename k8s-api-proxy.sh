63c3710d9070ef0136da391f628ce0e92adaa4a1
Leandro Silva <leandro.silva@hurb.com>
2023-04-19 17:29:16 -0300

    Including a ENV if you want use another K8S IP

--- k8s-api-proxy.sh
+++ k8s-api-proxy.sh
@@ -5,18 +5,16 @@ set -o pipefail
 set -o nounset
 
 # Get the internal cluster IP
-# export TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
-# INTERNAL_IP=$(curl -H "Authorization: Bearer $TOKEN" -k -SsL https://kubernetes.default/api |
-# jq -r '.serverAddressByClientCIDRs[0].serverAddress')
-
-# Get external IP (we call internal for tests purposes (= )
-INTERNAL_IP=$(curl -SsL --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes.default/api |
- jq -r '.serverAddressByClientCIDRs[0].serverAddress')
+export TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+ 
+if [ -z ${K8S_IP} ]; then 
+K8S_IP=$(curl -H "Authorization: Bearer $TOKEN" -k -SsL https://kubernetes.default/api | jq -r '.serverAddressByClientCIDRs[0].serverAddress')
+fi
 
 # Replace CLUSTER_IP in the rewrite filter and action file
-sed -i "s/CLUSTER_IP/${INTERNAL_IP}/g"\
+sed -i "s/CLUSTER_IP/${K8S_IP}/g"\
  /etc/privoxy/k8s-rewrite-internal.filter
-sed -i "s/CLUSTER_IP/${INTERNAL_IP}/g"\
+sed -i "s/CLUSTER_IP/${K8S_IP}/g"\
  /etc/privoxy/k8s-only.action
 
 # Start Privoxy un-daemonized
