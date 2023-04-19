0b3eca9d9476ad730751603cf94163bbdfbc0375
Leandro Silva <leandro.silva@hurb.com>
2023-04-19 17:11:02 -0300

    Change to external IP to tests purposes

--- k8s-api-proxy.sh
+++ k8s-api-proxy.sh
@@ -5,9 +5,13 @@ set -o pipefail
 set -o nounset
 
 # Get the internal cluster IP
-export TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
-INTERNAL_IP=$(curl -H "Authorization: Bearer $TOKEN" -k -SsL https://kubernetes.default/api |
-jq -r '.serverAddressByClientCIDRs[0].serverAddress')
+# export TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+# INTERNAL_IP=$(curl -H "Authorization: Bearer $TOKEN" -k -SsL https://kubernetes.default/api |
+# jq -r '.serverAddressByClientCIDRs[0].serverAddress')
+
+# Get external IP (we call internal for tests purposes (= )
+INTERNAL_IP=$(curl -SsL --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes.default/api |
+ jq -r '.serverAddressByClientCIDRs[0].serverAddress')
 
 # Replace CLUSTER_IP in the rewrite filter and action file
 sed -i "s/CLUSTER_IP/${INTERNAL_IP}/g"\
